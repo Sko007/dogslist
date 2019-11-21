2fb9214559d44237548140e67ec296e5fca40625
Severin <severin_koch@gmx.de>
2019-11-21 12:11:59 +0100

    add

---
+++ src/components/DogsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DogsList extends Component {
+    render() {
+        return (
+            <div className="dogs-list">
+                <h1>Dogs list</h1>      
+            </div>
+        );
+    }
+}
+
+export default DogsList;
